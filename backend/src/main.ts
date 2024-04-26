e8fcbffa5b411d4ca0ca0af6787dd84ccda80615
Adrian <abaez9480@gmail.com>
2024-04-25 23:44:17 -0500

    Little exmple of file upload

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-  })),
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
+  console.log("App is running on :${await app.getUrl()}");
 }
 bootstrap();
