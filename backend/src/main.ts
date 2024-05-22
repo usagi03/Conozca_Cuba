a30ec53479f475fe2901838daf2c26dfe21eed9b
Adrian <abaez9480@gmail.com>
2024-05-22 00:28:48 -0500

    Cors config enabled

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
+import * as config from "config";
 
 async function bootstrap() {
+  const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  }
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
-  console.log("App is running on :${await app.getUrl()}");
+  const serverConfig = config.get("server");
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log("App is running on : ${port}");
 }
 bootstrap();
